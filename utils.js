988bed88c2985cb5538bdfba869c6f01b4e1f3bf
Pim <pim@vicompany.nl>
2018-08-22 14:47:31 +0200

    add radian stuff

--- utils.js
+++ utils.js
@@ -2,11 +2,15 @@
 const qs = sel => document.querySelector(sel);
 const map = (value, start1, stop1, start2, stop2) => ((value - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
 
+const toRadian = degrees => degrees * Math.PI / 180;
+const toDegrees = radians => radians * 180 / Math.PI;
+
 const pixelIndex = (x, y, imageData) => (~~x + ~~y * imageData.width) * 4;
 const distanceBetween = (vec1, vec2) => Math.hypot(vec2.x - vec1.x, vec2.y - vec1.y);
 const angleBetween = (vec1, vec2) => Math.atan2(vec2.y - vec1.y, vec2.x - vec1.x);
 const randomArrayValue = arr => arr[Math.floor(Math.random() * arr.length)];
 const randomBetween = (min, max) => (Math.random() * (max - min + 1)) + min;
+
 const randomGaussian = (mean = 0, sd = 1) => {
   let y1;
   let y2;
@@ -50,6 +54,8 @@ const wrappBBox = (vec, w, h) => {
 export {
 	qs,
 	map,
+	toRadian,
+	toDegrees,
 	pixelIndex,
   distanceBetween,
   angleBetween,
